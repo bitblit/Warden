2f980d22185a528d9e766b759fba73a9900288a3
Christopher Weiss <bitblit@gmail.com>
2023-02-09 11:48:41 -0700

    Remove sync

--- src/client/warden-user-service.ts
+++ src/client/warden-user-service.ts
@@ -55,7 +55,7 @@ export class WardenUserService<T> {
   public async checkForAutoLogoutOrRefresh(t: number): Promise<void> {
     Logger.debug('Checking for auto-logout or refresh : %s', t);
     // This code will cause an auto-logout if the token is already expired, but not if its CLOSE to expiration
-    const current: WardenLoggedInUserWrapper<T> = await this.fetchLoggedInUserWrapper();
+    const current: WardenLoggedInUserWrapper<T> = this.fetchLoggedInUserWrapper();
     if (current) {
       const thresholdSeconds: number = this.options.autoLoginHandlingThresholdSeconds || 10; // Default to 10 seconds
       const secondsLeft: number = current.expirationEpochSeconds - Math.floor(Date.now() / 1000);
@@ -66,7 +66,7 @@ export class WardenUserService<T> {
           this.options.eventProcessor.onAutomaticTokenRefresh(result);
         } else {
           Logger.info('Under threshold, initiating auto-logout');
-          await this.logout();
+          this.logout();
         }
       }
     }
@@ -82,57 +82,57 @@ export class WardenUserService<T> {
     return rval;
   }
 
-  public async fetchLoggedInUserWrapper(): Promise<WardenLoggedInUserWrapper<T>> {
+  public fetchLoggedInUserWrapper(): WardenLoggedInUserWrapper<T> {
     let tmp: WardenLoggedInUserWrapper<T> = this.options.loggedInUserProvider.fetchLoggedInUserWrapper();
     if (tmp) {
       if (WardenUserService.wrapperIsExpired(tmp)) {
         // This is belt-and-suspenders for when the window was not open - during normal operation either
         // auto-logout thread or auto-refresh thread would have handled this
         Logger.info('Token is expired - auto logout triggered');
-        await this.logout();
+        this.logout();
         tmp = null;
       }
     }
     return tmp;
   }
 
-  public async loggedInUserHasRole(role: string): Promise<boolean> {
+  public loggedInUserHasRole(role: string): boolean {
     let rval: boolean = false;
     if (StringRatchet.trimToNull(role)) {
-      const t: WardenLoggedInUserWrapper<T> = await this.fetchLoggedInUserWrapper();
+      const t: WardenLoggedInUserWrapper<T> = this.fetchLoggedInUserWrapper();
       const testRole: string = role.toLowerCase();
       rval = t?.userObject?.roles && !!t.userObject.roles.find((r) => r.toLowerCase() === testRole);
     }
     return rval;
   }
 
-  public async isLoggedIn(): Promise<boolean> {
-    const t: WardenLoggedInUserWrapper<T> = await this.fetchLoggedInUserWrapper();
+  public isLoggedIn(): boolean {
+    const t: WardenLoggedInUserWrapper<T> = this.fetchLoggedInUserWrapper();
     return !!t;
   }
 
-  public async fetchLoggedInUserJwtObject(): Promise<WardenJwtToken<T>> {
-    const t: WardenLoggedInUserWrapper<T> = await this.fetchLoggedInUserWrapper();
+  public fetchLoggedInUserJwtObject(): WardenJwtToken<T> {
+    const t: WardenLoggedInUserWrapper<T> = this.fetchLoggedInUserWrapper();
     return t ? t.userObject : null;
   }
 
-  public async fetchLoggedInUserJwtToken(): Promise<string> {
-    const t: WardenLoggedInUserWrapper<T> = await this.fetchLoggedInUserWrapper();
+  public fetchLoggedInUserJwtToken(): string {
+    const t: WardenLoggedInUserWrapper<T> = this.fetchLoggedInUserWrapper();
     return t ? t.jwtToken : null;
   }
 
-  public async fetchLoggedInUserObject(): Promise<T> {
-    const t: WardenJwtToken<T> = await this.fetchLoggedInUserJwtObject();
+  public fetchLoggedInUserObject(): T {
+    const t: WardenJwtToken<T> = this.fetchLoggedInUserJwtObject();
     return t ? t.user : null;
   }
 
-  public async fetchLoggedInUserExpirationEpochSeconds(): Promise<number> {
-    const t: WardenJwtToken<T> = await this.fetchLoggedInUserJwtObject();
+  public fetchLoggedInUserExpirationEpochSeconds(): number {
+    const t: WardenJwtToken<T> = this.fetchLoggedInUserJwtObject();
     return t ? t.exp : null;
   }
 
-  public async fetchLoggedInUserRemainingSeconds(): Promise<number> {
-    const t: WardenJwtToken<T> = await this.fetchLoggedInUserJwtObject();
+  public fetchLoggedInUserRemainingSeconds(): number {
+    const t: WardenJwtToken<T> = this.fetchLoggedInUserJwtObject();
     return t ? t.exp - Math.floor(Date.now() / 1000) : null;
   }
 
@@ -160,7 +160,7 @@ export class WardenUserService<T> {
 
   public async refreshToken(): Promise<WardenLoggedInUserWrapper<T>> {
     let rval: WardenLoggedInUserWrapper<T> = null;
-    const currentWrapper: WardenLoggedInUserWrapper<T> = await this.fetchLoggedInUserWrapper();
+    const currentWrapper: WardenLoggedInUserWrapper<T> = this.fetchLoggedInUserWrapper();
     if (!currentWrapper) {
       Logger.info('Could not refresh - no token available');
     } else {
